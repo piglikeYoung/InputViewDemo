667804dc56e13e16ff5fd8ff857c98c8e4b166e1
piglikeyoung <316046510@qq.com>
2015-11-17 17:27:33 +0800

    Initial Commit

---
+++ InputViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InputViewDemo
+//
+//  Created by piglikeyoung on 15/11/17.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InputViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InputViewDemo
+//
+//  Created by piglikeyoung on 15/11/17.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
